c77fc4765e1d9d504963990b0d306d82f797804b
Sergiy Poplavskyi <s.poplavskiy@outlook.com>
2018-10-09 16:44:37 -0700

    Initial Commit
    Project sceleton has been created

---
+++ Actions/Actions.cs
@@ -0,0 +1,49 @@
+﻿using Objects;
+using System;
+using System.Threading.Tasks;
+
+namespace Actions
+{
+    public class Actions
+    {
+    }
+
+    public class Dummy
+    {
+        public Task uploadFile(UploadFileParams @params)
+        {
+            return Task.Delay(1000);
+        }
+
+        public Task downloadFile(DownloadFileParams @params)
+        {
+            return Task.Delay(1000);
+        }
+
+        public Task<byte[]> getFile(GetFileParams @params)
+        {
+            return Task.Run(() => new byte[1024]);
+        }
+
+        public Task writeFile(WriteFileParams @params)
+        {
+            return Task.Delay(1000);
+        }
+
+        public Task removeFile(RemoveFileParams @params)
+        {
+            return Task.Delay(1000);
+        }
+
+        public Task truncateFile(TruncateFileParams @params)
+        {
+            return Task.Delay(1000);
+        }
+
+        public Task listFile(ListFileParams @params)
+        {
+            return Task.Run(() => new string[3] {"file1.txt", "file2.txt", "file3.txt" });
+        }
+
+    }
+}
---
+++ Objects/Objects.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Objects
+{
+    public class UploadFileParams
+    {
+        public string sourcePath { get; set; }
+        public string sourceFilename { get; set; }
+        public string containerName { get; set; }
+        public string sasUri { get; set; }
+        public string targetFilename { get; set; }
+        public string contentType { get; set; }
+        public bool append { get; set; }
+    }
+
+    public class DownloadFileParams
+    {
+        public string targetPath { get; set; }
+        public string targetFilename { get; set; }
+        public string containerName { get; set; }
+        public string sasUri { get; set; }
+        public string filename { get; set; }
+        public bool append { get; set; }
+    }
+
+    public class GetFileParams
+    {
+        public string sourcePath { get; set; }
+        public string sourceFilename { get; set; }
+    }
+
+    public class WriteFileParams
+    {
+        public string sourcePath { get; set; }
+        public string sourceFilename { get; set; }
+        public bool append { get; set; }
+        public byte[] data { get; set; }
+    }
+
+    public class RemoveFileParams
+    {
+        public string sourcePath { get; set; }
+        public string sourceFilename { get; set; }
+    }
+
+    public class TruncateFileParams
+    {
+        public string sourcePath { get; set; }
+        public string sourceFilename { get; set; }
+        public int maxRows { get; set; }
+    }
+
+    public class ListFileParams
+    {
+        public string sourcePath { get; set; }
+        public string sourceFilename { get; set; }
+        public int maxRows { get; set; }
+    }
+}
