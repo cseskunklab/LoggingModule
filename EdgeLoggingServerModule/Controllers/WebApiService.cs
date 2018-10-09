8cdf14175c3e1b7ec317e861e53eafdfcc698b4f
Tara Walker <tawalker@microsoft.com>
2018-10-09 16:35:19 -0700

    Committing HTTPServer Module work in progress-TEW

---
+++ EdgeLoggingServerModule/Controllers/WebApiService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FieldGatewayMicroservice.Controllers
+{
+    public abstract class WebApiService<IService> : ControllerBase
+    {
+        protected IService InnerService
+        {
+            get;
+            private set;
+        }
+
+        public WebApiService(IService service)
+        {
+            InnerService = service;
+        }
+    }
+}
