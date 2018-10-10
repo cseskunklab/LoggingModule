00b75d2cb4f4631be8608ef3dff8826d751ceece
Mary Sha <meisha@microsoft.com>
2018-10-10 15:09:13 -0700

    Add extra download and truncate

--- Interfaces/LogModule.Core/IRemote.cs
+++ Interfaces/LogModule.Core/IRemote.cs
@@ -8,5 +8,7 @@ namespace LogModule.Core
         Task UploadFile(string sourcePath, string sourceFilename, string containerName, string targetFilename, string contentType, bool append = false);
         Task UploadFile(string sourcePath, string sourceFilename, string sasUri, string contentType, bool append = false);
         Task DownloadFile(string targetPath, string targetFilename, string containerName, string filename, bool append = false);
+        Task DownloadFile(string targetPath, string targetFilename, string sasUri, bool append = false);
+        Task TruncateFile(string sourcePath, string sourceFilename, int maxBytes);
     }
 }
