6933af3fe7f465f1a6febb521d21a6a603437e3c
Munish Malhotra <munishm@outlook.com>
2018-10-10 21:59:59 +0530

    added logModule.core
    added logModule.core

---
+++ Interfaces/LogModule.Core/IRemote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LogModule.Core
+{
+    public interface IRemote
+    {
+        Task UploadFile(string sourcePath, string sourceFilename, string containerName, string targetFilename, string contentType, bool append = false);
+        Task UploadFile(string sourcePath, string sourceFilename, string sasUri, string contentType, bool append = false);
+
+        Task DownloadFile(string targetPath, string targetFilename, string containerName, string filename, bool append = false);
+
+        
+    }
+}
