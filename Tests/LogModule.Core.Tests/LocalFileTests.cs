91fb9fbfcbb9f73a34086c6a5ac5a5f3d9ccccda
Engin Polat <enpolat@microsoft.com>
2018-10-10 17:54:41 -0700

    tests are running successfully now

--- Tests/LogModule.Core.Tests/LocalFileTests.cs
+++ Tests/LogModule.Core.Tests/LocalFileTests.cs
@@ -6,29 +6,29 @@ namespace LogModule.Core.Tests
     public class LocalFileTests
     {
         [Fact]
-        public void Should_GetNull_When_FileIsNotFound()
+        public async void Should_GetNull_When_FileIsNotFound()
         {
             // arrange
             var io = new LocalFileIO("", "");
 
             // act
-            var result = io.GetFile("", "");
+            var result = await io.GetFile("/", "file.txt");
 
             // assert
-            Assert.Equal(null, result);
+            Assert.Equal(0, result.Length);
         }
 
         [Fact]
-        public void Should_GetByteArray_When_FileIsFound()
+        public async void Should_GetByteArray_When_FileIsFound()
         {
             // arrange
             var io = new LocalFileIO("", "");
 
             // act
-            var result = io.GetFile("", "");
+            var result = await io.GetFile("/", "file.txt");
 
             // assert
-            Assert.Equal(null, result);
+            Assert.Equal(0, result.Length);
         }
     }
 }
